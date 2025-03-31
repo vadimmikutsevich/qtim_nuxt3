eff6078e07c61f5a6333b9e80996a619dab52b22
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2025-04-01 02:46:16 +0400

    fix for building

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<!-- pages/about.vue -->
+<template>
+  <div class="p-8 text-center">
+    <h1>About Page</h1>
+    <p>This is a placeholder page.</p>
+  </div>
+</template>
---
+++ pages/works.vue
@@ -0,0 +1,7 @@
+<!-- pages/works.vue -->
+<template>
+  <div class="p-8 text-center">
+    <h1>Works Page</h1>
+    <p>This is a placeholder page.</p>
+  </div>
+</template>
